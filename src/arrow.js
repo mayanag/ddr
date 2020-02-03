471797b824f645e16ca0c41d1648137cbf98e863
maya <mayanag@ucla.edu>
2020-02-02 21:17:46 -0800

    multipel arrows animating

--- src/arrow.js
+++ src/arrow.js
@@ -1,8 +1,8 @@
 const TYPES = {
   0: { startX: 100, imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png'},
-  1: { startX: 300,  imgUrl: '../assets/krabs.png'},
-  2: { startX: 500,  imgUrl: '../assets/patrick.png'},
-  3: { startX: 700,  imgUrl: '../assets/squid.png'}
+  1: { startX: 300,  imgUrl: './assets/krabs.png'},
+  2: { startX: 500,  imgUrl: './assets/patrick.png'},
+  3: { startX: 700,  imgUrl: './assets/squid.png'}
 };
 
 const NORMAL_FRAME_TIME_DELTA = 1000 / 60;
@@ -15,7 +15,6 @@ class MovingArrow {
     this.x = this.typeObj.startX;//TYPES[type].startPos[0],
     this.y = 0 //TYPES[type].startPos[1]
     this.image = this.typeObj.imgUrl;
-    this.ctx = ctx;
   };
 
   getType() {
@@ -27,34 +26,7 @@ class MovingArrow {
   };
 
 
-  draw(id, ctx) {
-    //ctx.fillRect(this.x, this.y, 20, 20);
-    // let img = new Image;
-    // img.src = this.image; 
-    // img.id = id;
-    // img.onload = function() {
-    //   ctx.drawImage(img, this.x, this.y,50, 50);
-    // };
-    var imgTag = new Image();
-    var canvas = document.getElementById('game-canvas');
-    var ctx = canvas.getContext("2d");
-    var x = this.x;
-    var y = this.y;
-
-    imgTag.onload = animate;
-    imgTag.src = this.image;   // load image
-
-    function animate() {
-      setTimeout(function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
-        ctx.drawImage(imgTag, x, y, 20, 20);                       // draw image at current position
-        y += 5;
-        if (y < canvas.height) requestAnimationFrame(animate) 
-      },100);
-             // loop
-    }
-    requestAnimationFrame(animate);
-  }
+  
 
   
   
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const Game = require('./game');
 const Gameview = require('./gameview');
+const MovingArrow = require('./arrow');
 var gameView ;
+var movingArrows = [];
 console.log("Webpack is working!");
 
 
@@ -10,9 +12,19 @@ document.addEventListener('DOMContentLoaded', function () {
   canvasEl.height = 500;
 
   const ctx = canvasEl.getContext("2d");
-  const game = new Game;
-  gameView = new Gameview(game, "game-canvas");
-  gameView.start();
+  // const game = new Game;
+  // gameView = new Gameview(game, "game-canvas");
+  // gameView.start();
+  initArrows();
+  var currentArrowIndex = 0;
+  var intervalId = setInterval(function() {
+    if (currentArrowIndex>= movingArrows.length) {
+      clearInterval(intervalId);
+      return;
+    }
+    draw(currentArrowIndex)
+    currentArrowIndex++;
+  }, 1000);
   // ctx.beginPath();
   // ctx.rect(20,20, 150, 100);
   // ctx.stroke();
@@ -20,4 +32,33 @@ document.addEventListener('DOMContentLoaded', function () {
  
 });
 
-export {gameView};
+
+function initArrows() {
+
+  movingArrows.push(new MovingArrow(0, 5, null));
+  movingArrows.push(new MovingArrow(1, 5, null));
+  movingArrows.push(new MovingArrow(2, 5, null));
+  movingArrows.push(new MovingArrow(3, 5, null));
+}
+function draw(arrowIndex) {
+  
+  var canvas = document.getElementById('game-canvas');
+  var ctx = canvas.getContext("2d");
+  var arrow = movingArrows[arrowIndex];
+  var imgTag = new Image();
+
+  imgTag.onload = animate;
+  imgTag.src = arrow.image;   // load image
+
+  function animate() {
+    setTimeout(function () {
+      ctx.clearRect(arrow.x, 0, 20, canvas.height);  // clear canvas
+      ctx.drawImage(imgTag, arrow.x, arrow.y, 20, 20);                       // draw image at current position
+      arrow.y += 5;
+      if (arrow.y < canvas.height) requestAnimationFrame(animate) 
+    },100);
+           // loop
+  }
+  requestAnimationFrame(animate);
+}
+
